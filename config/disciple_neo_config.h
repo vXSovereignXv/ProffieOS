cc4b9bfc2ec0e679fc85766f597e47c75fdc76e3
Nick Daigle <nickdaigle66@gmail.com>
2020-04-14 00:48:45 -0500

    Setting up Graflex config

--- config/disciple_neo_config.h
+++ config/disciple_neo_config.h
@@ -13,6 +13,7 @@ const unsigned int maxLedsPerStrip = 193;
 #define SHARED_POWER_PINS
 #define ENABLE_POWER_FOR_ID PowerPINS<bladePowerPin2,bladePowerPin3>
 #define BLADE_DETECT_PIN 8
+#define IDLE_OFF_TIME 60 * 5 * 1000 //5 minutes
 #endif
 
 #ifdef CONFIG_PROP
@@ -26,8 +27,10 @@ const unsigned int maxLedsPerStrip = 193;
 #define mainColors ColorChange<TrFade<500>, White, GreenYellow, Chartreuse, Green, SpringGreen, Cyan, DeepSkyBlue, DodgerBlue, Blue, Violet, Indigo, Magenta, HotPink, Red, Orange, Yellow>
 #define flickerColors ColorChange<TrFade<500>, Rgb<128, 128, 128>, Rgb<54, 128, 3>, Rgb<28, 128, 0>, Rgb<0, 128, 0>, SpringGreenDark, DeepSkyBlue, DeepSkyBlueDark, DodgerBlueDark, Rgb<0, 0, 128>, VioletDark, IndigoDark, MagentaDark, Pink, Rgb<128, 0, 0>, Rgb<128, 48, 0>, Rgb<128, 128, 0>>
 #define clashColors ColorChange<TrFade<500>, Cyan, White, White, White, White, White, White, White, White, White, White, White, White, White, White, White>
-#define mainBladeStyle InOutHelper<OnSpark<LocalizedClash<Lockup<BlastFadeout<AudioFlicker<mainColors,flickerColors>, clashColors>, RandomFlicker<mainColors,clashColors>>, clashColors, 40>, clashColors, 500>, 300, 800, Black>
-#define unstableStyle InOutHelper<Blast<LocalizedClash<Lockup<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<Mix<SwingSpeed<600>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,300>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<0,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,StyleFire<BrownNoiseFlicker<Red,Rgb<25,0,0>,300>,Rgb<128,0,0>,0,8,FireConfig<10,1200,1>,FireConfig<0,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>>,StyleFire<Red,Rgb<150,150,150>,0,6>,TrInstant,TrSmoothFade<800>,EFFECT_IGNITION>,Mix<Bump<Int<32768>,Int<16000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,RandomPerLEDFlicker<OrangeRed,White>>,TrConcat<TrWipeIn<600>,Mix<Bump<Int<32768>,Int<16000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,RandomPerLEDFlicker<Orange,White>>,TrWipe<600>>,TrInstant,EFFECT_STAB>,BrownNoiseFlicker<White,Red,400>,TrInstant,TrSmoothFade<200>,EFFECT_LOCKUP_BEGIN>,Mix<Bump<Int<32768>,Int<10000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,White>,TrInstant,TrFade<200>,EFFECT_DRAG_BEGIN>,Mix<Bump<Int<32768>,Int<10000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,White>,TrInstant,TrFade<300>,EFFECT_DRAG_END>,Black,TrConcat<TrInstant,HumpFlicker<White,BrownNoiseFlicker<Red,Rgb<25,0,0>,400>,40>,TrColorCycle<600>>,TrInstant,EFFECT_RETRACTION>,Mix<Bump<Int<0>,Int<44000>>,Mix<Bump<Int<32768>,Int<44000>>,TransitionEffect<Red,White,TrInstant,TrFade<300>,EFFECT_LOCKUP_END>,Stripes<1000,-1500,Red,BrownNoiseFlicker<White,Red,300>>>,Stripes<1000,1500,Red,BrownNoiseFlicker<White,Red,300>>>,TrInstant,TrSmoothFade<800>,EFFECT_LOCKUP_END>,Pulsing<Gradient<BrownNoiseFlicker<Red,Rgb<25,0,0>,400>,BrownNoiseFlicker<White,Strobe<Blue,White,50,1>,100>>,Gradient<BrownNoiseFlicker<White,Strobe<Blue,White,50,1>,100>,BrownNoiseFlicker<Red,Rgb<25,0,0>,400>>,3500>,RandomPerLEDFlicker<BrownNoiseFlicker<Red,Rgb<25,0,0>,400>,White>,Bump<Int<16000>,Int<16000>>,Bump<Int<32768>,Int<10000>>>,White>,White>,150,300,Black>
+#define mainBladeStyle InOutHelper<InOutSparkTip<LocalizedClash<TransitionEffect<TransitionEffect<Lockup<Blast<AudioFlicker<mainColors,flickerColors>, clashColors>, RandomFlicker<mainColors,clashColors>>, RandomFlicker<mainColors,clashColors>, TrFade<50>, TrFade<50>, EFFECT_LOCKUP_BEGIN>, RandomFlicker<mainColors,clashColors>, TrFade<100>, TrFade<100>, EFFECT_LOCKUP_END>, clashColors, 40>, 500, 800>, 500, 800, Black>
+#define unstableStyle InOutHelper<Blast<LocalizedClash<Lockup<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<Mix<SwingSpeed<300>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,Mix<SwingSpeed<500>,BrownNoiseFlicker<OrangeRed,Rgb<80,40,40>,300>,StyleFire<BrownNoiseFlicker<OrangeRed,RandomPerLEDFlicker<Rgb<25,5,5>,Rgb<60,20,20>>,200>,Rgb<80,40,40>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>>>,StyleFire<White,Red,0,6>,TrInstant,TrSmoothFade<1600>,EFFECT_IGNITION>,Mix<Bump<Int<32768>,Int<16000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,RandomPerLEDFlicker<OrangeRed,White>>,TrConcat<TrWipeIn<600>,Mix<Bump<Int<32768>,Int<16000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,RandomPerLEDFlicker<Orange,White>>,TrWipe<600>>,TrInstant,EFFECT_STAB>,White,TrInstant,TrSmoothFade<200>,EFFECT_LOCKUP_BEGIN>,StyleFire<Red,White,0,6>,TrInstant,TrSmoothFade<500>,EFFECT_LOCKUP_END>,Mix<Bump<Int<32768>,Int<10000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,White>,TrInstant,TrFade<200>,EFFECT_DRAG_BEGIN>,Mix<Bump<Int<32768>,Int<10000>>,StyleFire<BrownNoiseFlicker<Red,RandomPerLEDFlicker<Rgb<25,0,0>,Rgb<60,0,0>>,200>,Rgb<80,0,0>,0,6,FireConfig<10,1000,2>,FireConfig<4000,0,0>,FireConfig<2,1000,5>,FireConfig<100,0,5>>,White>,TrInstant,TrFade<300>,EFFECT_DRAG_END>,Black,TrConcat<TrInstant,HumpFlicker<White,Red,40>,TrColorCycle<600>>,TrInstant,EFFECT_RETRACTION>,Pulsing<Gradient<Red,BrownNoiseFlicker<White,Strobe<Blue,White,50,1>,100>>,Gradient<BrownNoiseFlicker<White,Strobe<Blue,White,50,1>,100>,Red>,3500>,RandomPerLEDFlicker<Red,White>,Bump<Int<16000>,Int<20000>>,Bump<Int<32768>,Int<10000>>>,White>,White>,300,500,Black>
+#define fettAudioFlicker InOutHelper<Blast<LocalizedClash<Lockup<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<Mix<SwingSpeed<400>,AudioFlicker<DeepSkyBlue,SteelBlue>,AudioFlicker<Blue,Rgb<0,128,0>>>,StyleFire<White,DeepSkyBlue,0,6>,TrInstant,TrSmoothFade<1600>,EFFECT_IGNITION>,Mix<Bump<Int<32768>,Int<16000>>,AudioFlicker<DeepSkyBlue,SteelBlue>,RandomPerLEDFlicker<OrangeRed,White>>,TrConcat<TrWipeIn<600>,Mix<Bump<Int<32768>,Int<16000>>,AudioFlicker<DeepSkyBlue,SteelBlue>,RandomPerLEDFlicker<Orange,White>>,TrWipe<600>>,TrInstant,EFFECT_STAB>,White,TrInstant,TrSmoothFade<200>,EFFECT_LOCKUP_BEGIN>,StyleFire<DeepSkyBlue,White,0,6>,TrInstant,TrSmoothFade<500>,EFFECT_LOCKUP_END>,Mix<Bump<Int<32768>,Int<10000>>,AudioFlicker<DeepSkyBlue,SteelBlue>,White>,TrInstant,TrFade<200>,EFFECT_DRAG_BEGIN>,Mix<Bump<Int<32768>,Int<10000>>,AudioFlicker<DeepSkyBlue,SteelBlue>,White>,TrInstant,TrFade<300>,EFFECT_DRAG_END>,Black,TrConcat<TrInstant,HumpFlicker<White,DeepSkyBlue,40>,TrColorCycle<600>>,TrInstant,EFFECT_RETRACTION>,Pulsing<Gradient<DeepSkyBlue,BrownNoiseFlicker<White,Strobe<Blue,White,50,1>,100>>,Gradient<BrownNoiseFlicker<White,Strobe<Blue,White,50,1>,100>,DeepSkyBlue>,3500>,RandomPerLEDFlicker<DeepSkyBlue,White>,Bump<Int<16000>,Int<20000>>,Bump<Int<32768>,Int<10000>>>,White>,White>,300,500,Black>
+#define senateMajority InOutTr<Blast<BlastFadeout<LocalizedClash<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<Lockup<Mix<SwingSpeed<500>,AudioFlicker<Gradient<Gradient<BrownNoiseFlicker<Black,Orange,400>,Red>,Red,Red,Red,Red,Red,Red,Red>,Rgb<120,0,0>>,Gradient<BrownNoiseFlicker<Black,DarkOrange,400>,BrownNoiseFlicker<Black,Rgb<128,0,0>,400>,BrownNoiseFlicker<Black,Rgb<128,0,0>,400>,BrownNoiseFlicker<Black,Rgb<128,0,0>,400>>>,Pulsing<Gradient<Strobe<Black,BrownNoiseFlicker<SteelBlue,LightPink,400>,40,90>,RandomFlicker<Red,Rgb<150,20,40>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Red,Red>,Gradient<Strobe<Black,BrownNoiseFlicker<SteelBlue,LightPink,400>,40,90>,RandomFlicker<Red,Magenta>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Magenta,Red>,RandomFlicker<Red,Black>,RandomFlicker<Red,DarkOrange>>,3500>,RandomPerLEDFlicker<Red,White>>,White,TrWipeIn<50>,TrSmoothFade<50>,EFFECT_DRAG_END>,Gradient<Orange,Orange,White>,TrSmoothFade<50>,TrWipeIn<50>,EFFECT_DRAG_BEGIN>,Gradient<White,DeepSkyBlue,LightPink,DeepSkyBlue,White>,TrJoinR<TrWipe<200>,TrWipeIn<200>>,TrSmoothFade<300>,EFFECT_LOCKUP_END>,White,TrBoing<300,2>,TrFade<100>,EFFECT_LOCKUP_BEGIN>,BrownNoiseFlicker<White,DarkOrange,400>,100,75>,Rgb<120,0,0>>,White,200,200>,TrConcat<TrColorCycle<80,-400,-1000>,White,TrWipeIn<50>,Black,TrDelay<450>,Strobe<Black,White,45,60>,TrJoinR<TrSmoothFade<190>,TrWipe<200>>>,TrConcat<TrInstant,White,TrJoin<TrJoin<TrJoin<TrColorCycle<2050,-1500,-1>,TrBoing<2050,15>>,TrBoing<2050,18>>,TrSmoothFade<2050>>>>
 
 Preset blade[] = {
   { "Unstable", "tracks/KyloTheme.wav",
@@ -36,7 +39,7 @@ Preset blade[] = {
     StylePtr<buttonPulse>(), "Unstable"
   },
   { "FOSister", "tracks/JFO12.wav",
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<chassisOff>(),
     StylePtr<buttonPulse>(), "FOSister"
   },
@@ -46,29 +49,34 @@ Preset blade[] = {
     StylePtr<buttonPulse>(), "Fulcrum"
   },
   { "Hero_Obi", "Hero_Obi/tracks/BattleOfHeros.wav",
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<chassisOff>(),
     StylePtr<buttonPulse>(), "Hero_Obi"
   },
   { "Luke_ESB", "Luke_ESB/tracks/LvsV.wav",
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<chassisOff>(),
     StylePtr<buttonPulse>(), "Luke_ESB"
   },
   { "Luke_RoTJ", "Luke_RoTJ/tracks/Final_Duel.wav",
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<chassisOff>(),
     StylePtr<buttonPulse>(), "Luke_RoTJ"
   },
   { "ClassicVillian", "Luke_ESB/tracks/LvsV.wav",
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<chassisOff>(),
     StylePtr<buttonPulse>(), "ClassicVillian"
   },
   { "Scavengers_Destiny", "Scavengers_Destiny/tracks/track4.wav",
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<chassisOff>(),
     StylePtr<buttonPulse>(), "Scavengers_Destiny"
+  },
+  { "Senate Majority", "Senate Majority/tracks/Palpatheme.wav",
+    StylePtr<senateMajority>(),
+    StylePtr<chassisOff>(),
+    StylePtr<buttonPulse>(), "Senate Majority"
   }
 };
 
@@ -79,8 +87,8 @@ Preset chassis[] = {
     StylePtr<buttonPulse>(), "Unstable"
   },
   { "FOSister", "tracks/JFO12.wav",
-    StylePtr<mainBladeStyle>(),
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<buttonPulse>(), "FOSister"
   },
   { "Fulcrum", "fulcrum/tracks/track3.wav",
@@ -89,29 +97,34 @@ Preset chassis[] = {
     StylePtr<buttonPulse>(), "Fulcrum"
   },
   { "Hero_Obi", "Hero_Obi/tracks/BattleOfHeros.wav",
-    StylePtr<mainBladeStyle>(),
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<buttonPulse>(), "Hero_Obi"
   },
   { "Luke_ESB", "Luke_ESB/tracks/LvsV.wav",
-    StylePtr<mainBladeStyle>(),
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<buttonPulse>(), "Luke_ESB"
   },
   { "Luke_RoTJ", "Luke_RoTJ/tracks/Final_Duel.wav",
-    StylePtr<mainBladeStyle>(),
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<buttonPulse>(), "Luke_RoTJ"
   },
   { "ClassicVillian", "Luke_ESB/tracks/LvsV.wav",
-    StylePtr<mainBladeStyle>(),
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<buttonPulse>(), "ClassicVillian"
   },
   { "Scavengers_Destiny", "Scavengers_Destiny/tracks/track4.wav",
-    StylePtr<mainBladeStyle>(),
-    StylePtr<mainBladeStyle>(),
+    StylePtr<fettAudioFlicker>(),
+    StylePtr<fettAudioFlicker>(),
     StylePtr<buttonPulse>(), "Scavengers_Destiny"
+  },
+  { "Senate Majority", "Senate Majority/tracks/Palpatheme.wav",
+    StylePtr<senateMajority>(),
+    StylePtr<senateMajority>(),
+    StylePtr<buttonPulse>(), "Senate Majority"
   }
 };
 
--- props/saber_nd_buttons.h
+++ props/saber_nd_buttons.h
@@ -28,20 +28,26 @@
 // Force Effects - short click Activation button while ON
 // Enter Color Change mode - hold AUX and click Activation button while ON
 // Confirm selected color in Color Change mode - hold the button until confirmation sound
+// Volume UP - single click power button while OFF and in VOLUME MENU
+// Volume Down - single click aux button while OFF and in VOLUME MENU
+// Volume Menu - Twist while off to enter menu and twist again to exit (can also hold power button to exit)
 //
 // 3 Buttons: TODO
 // Activate Muted - fast double click Activation button while OFF
 // Activate blade - short click Activation button while OFF or stab gesture while OFF
 // Play/Stop Music - hold 1 second and release Activation button while OFF or ON
 // Turn the blade off - hold and wait till blade is off while ON (like in Plecter boards) or twist gesture while ON
-// Next Preset - short click AUX button while OFF
-// Previous Preset - hold AUX and click Activation button while OFF
+// Next Preset - short click AUX 1 button while OFF
+// Previous Preset - short click AUX 2 button while OFF
 // Lockup - hold AUX button while ON (like in Plecter boards)
 // Drag - hold AUX button while ON pointing the blade tip down
 // Blaster Blocks - short click AUX button while ON
 // Force Effects - short click Activation button while ON
 // Enter Color Change mode - hold AUX and click Activation button while ON
 // Confirm selected color in Color Change mode - hold the button until confirmation sound
+// Volume UP - single click aux button while OFF and in VOLUME MENU
+// Volume Down - single click aux2 button while OFF and in VOLUME MENU
+// Volume Menu - Hold AUX 2 and click Power Button while off, do the same to exit (can also click power button to exit)
 //
 
 #ifndef PROPS_SABER_DAIGLE_BUTTONS_H
@@ -272,19 +278,47 @@ public:
       case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_OFF):
       case EVENTID(BUTTON_AUX, EVENT_LATCH_ON, MODE_OFF):
       case EVENTID(BUTTON_AUX2, EVENT_LATCH_ON, MODE_OFF):
-      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF):
-        aux_on_ = false;
-        On();
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF):    
+        if (!mode_volume_) {
+          mode_volume_ = false;
+          aux_on_ = false;
+          On();
+        }
+        else {
+          ChangeVolume(true);
+        }
 	return true;
 
 // Next Preset
       case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_OFF):
 #ifdef DUAL_POWER_BUTTONS
-        aux_on_ = true;
-        On();
+        if (!mode_volume_) {
+          mode_volume_ = false;
+          aux_on_ = true;
+          On();
+        }
+        else {
+          ChangeVolume(false);
+        }
 #else
-        next_preset();
+        if (!mode_volume_) {
+          next_preset();
+        }
+        else {
+          ChangeVolume(false);
+        }
 #endif
+	return true;
+
+// Alternate volume menu exit
+      case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_OFF):
+        if (mode_volume_) {
+          ToggleVolumeMenu();
+        }
+	return true;
+// Enter Volume Menu
+      case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_OFF):
+        ToggleVolumeMenu();
 	return true;
 
 // Activate Muted
@@ -364,16 +398,155 @@ public:
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF | BUTTON_AUX):
         previous_preset();
 	return true;
+#elif NUM_BUTTONS == 3 //TODO: 3 button setup
+// Enter Volume Menu
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF | BUTTON_AUX2):
+        ToggleVolumeMenu();
+	return true;
+
+// Turn Blade ON
+      case EVENTID(BUTTON_POWER, EVENT_LATCH_ON, MODE_OFF):
+      case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_OFF):
+      case EVENTID(BUTTON_AUX, EVENT_LATCH_ON, MODE_OFF):
+      case EVENTID(BUTTON_AUX2, EVENT_LATCH_ON, MODE_OFF):
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF):
+        if (!mode_volume_) {
+          mode_volume_ = false;
+          aux_on_ = false;
+          On();
+        }
+        else {
+          ToggleVolumeMenu();
+        }
+	return true;
+
+// Next Preset
+      case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_OFF):
+#ifdef DUAL_POWER_BUTTONS        
+        if (!mode_volume_) {
+          mode_volume_ = false;
+          aux_on_ = true;
+          On();
+        }
+        else {
+          ChangeVolume(true);
+        }
+#else
+        if (!mode_volume_) {
+          next_preset();
+        }
+        else {
+          ChangeVolume(true);
+        }
+#endif
+	return true;
+
+// Activate Muted
+      case EVENTID(BUTTON_POWER, EVENT_DOUBLE_CLICK, MODE_ON):
+	if (millis() - activated_ < 500) {
+	  if (SetMute(true)) {
+	    unmute_on_deactivation_ = true;
+	  }
+	}
+	return true;
+
+// Alternate volume menu exit
+      case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_OFF):
+        if (mode_volume_) {
+          ToggleVolumeMenu();
+        }
+	return true;
+
+// Turn Blade OFF	
+      case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_ON):
+      case EVENTID(BUTTON_AUX, EVENT_LATCH_OFF, MODE_ON):
+      case EVENTID(BUTTON_AUX2, EVENT_LATCH_OFF, MODE_ON):
+      case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_ON):
+    if (!SaberBase::Lockup()) {
+#ifndef DISABLE_COLOR_CHANGE
+          if (SaberBase::GetColorChangeMode() != SaberBase::COLOR_CHANGE_MODE_NONE) {
+            // Just exit color change mode.
+            // Don't turn saber off.
+            ToggleColorChangeMode();
+            return true;
+          }
+#endif
+      Off();
+    }
+        return true;
+
+// Force
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON):
+        SaberBase::DoForce();
+	return true;
+
+// Color Change mode
+#ifndef DISABLE_COLOR_CHANGE
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON | BUTTON_AUX):
+	ToggleColorChangeMode();
+	break;
+#endif
+
+// Blaster Deflection
+      case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_ON):
+        SaberBase::DoBlast();
+	return true;
+
+// Lockup
+      case EVENTID(BUTTON_AUX, EVENT_HELD, MODE_ON):
+      case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_AUX):
+        if (!SaberBase::Lockup()) {
+          if (pointing_down_) {
+            SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
+          } else {
+            SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
+          }
+          SaberBase::DoBeginLockup();
+	  return true;
+        }
+        break;
+
+// Start or Stop Track
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_OFF):
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_ON):
+        StartOrStopTrack();
+	return true;
+
+      case EVENTID(BUTTON_POWER, EVENT_PRESSED, MODE_OFF):
+        SaberBase::RequestMotion();
+	return true;
+
+      case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_OFF | BUTTON_POWER):
+        next_preset();
+	return true;
+
+
+// Previous Preset
+      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF | BUTTON_AUX):
+        if (!mode_volume_) {
+          previous_preset();
+        }        
+	return true;
 
 #else
-#error only 1 and 2 buttons are supported by this mod
+  #error No buttons is not supported by this mod
 #endif
 
       case EVENTID(BUTTON_AUX2, EVENT_CLICK_SHORT, MODE_OFF):
-#ifdef DUAL_POWER_BUTTONS
-        next_preset();
+#ifdef DUAL_POWER_BUTTONS 
+        if (!mode_volume_) {
+          next_preset();
+        }
+        else {
+          ChangeVolume(true);
+        }
 #else
-        previous_preset();
+        if (!mode_volume_) {
+          previous_preset();
+        }
+        else {
+          ChangeVolume(false);
+        }
 #endif
 	return true;
 
