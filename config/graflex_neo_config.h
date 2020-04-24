9cb642a34dcd31e4bbad1d64d877b4f4eb1ec37f
Nick Daigle <nickdaigle66@gmail.com>
2020-04-23 21:09:53 -0500

    Update Graflex Config

--- config/graflex_neo_config.h
+++ config/graflex_neo_config.h
@@ -23,10 +23,6 @@ const unsigned int maxLedsPerStrip = 193;
 #ifdef CONFIG_PRESETS
 
 #define chassisOff Black
-#define mainColors ColorChange<TrFade<500>, White, GreenYellow, Chartreuse, Green, SpringGreen, Cyan, DeepSkyBlue, DodgerBlue, Blue, Violet, Indigo, Magenta, HotPink, Red, Orange, Yellow>
-#define flickerColors ColorChange<TrFade<500>, Rgb<128, 128, 128>, Rgb<54, 128, 3>, Rgb<28, 128, 0>, Rgb<0, 128, 0>, SpringGreenDark, DeepSkyBlue, DeepSkyBlueDark, DodgerBlueDark, Rgb<0, 0, 128>, VioletDark, IndigoDark, MagentaDark, Pink, Rgb<128, 0, 0>, Rgb<128, 48, 0>, Rgb<128, 128, 0>>
-#define clashColors ColorChange<TrFade<500>, Cyan, White, White, White, White, White, White, White, White, White, White, White, White, White, White, White>
-#define mainBladeStyle InOutHelper<InOutSparkTip<LocalizedClash<TransitionEffect<TransitionEffect<Lockup<Blast<AudioFlicker<mainColors,flickerColors>, clashColors>, RandomFlicker<mainColors,clashColors>>, RandomFlicker<mainColors,clashColors>, TrFade<50>, TrFade<50>, EFFECT_LOCKUP_BEGIN>, RandomFlicker<mainColors,clashColors>, TrFade<100>, TrFade<100>, EFFECT_LOCKUP_END>, clashColors, 40>, 500, 800>, 500, 800, Black>
 #define unstableStyle InOutTr<Layers<StyleFire<BrownNoiseFlicker<RotateColorsX<Variation,Red>,RandomPerLEDFlicker<RotateColorsX<Variation,Rgb<25,0,0>>,RotateColorsX<Variation,Rgb<60,0,0>>>,300>, RotateColorsX<Variation,Rgb<80,0,0>>,0,6,FireConfig<10,1000,2>,FireConfig<10,1000,2>,FireConfig<10,1000,2>,FireConfig<10,1000,2>>,TransitionEffectL<TrConcat<TrFade<400>,Mix<SwingSpeed<600>,AudioFlickerL<Rgb<150,0,0>>,Red>,TrDelay<30000>,Mix<SwingSpeed<600>,AudioFlickerL<Rgb<150,0,0>>,Red>,TrFade<800>>,EFFECT_FORCE>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Blue,300>,50,1>,TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,AlphaL<White,Bump<Int<0>,Scale<TwistAngle<>,Int<2000>,Int<10000>>>>,ResponsiveBlastFadeL<White>,ResponsiveBlastWaveL<White,300,100,300,26000,4000,EFFECT_CLASH>,TransitionEffectL<TrConcat<TrInstant,StyleFire<White,Rgb<40,40,40>,0,6>,TrFade<800>>,EFFECT_IGNITION>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<1000>>,EFFECT_RETRACTION>,ResponsiveStabL<OrangeRed,TrWipeIn<600>,TrWipe<600>>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,OrangeRed,Orange>,TrWipeIn<600>,TrSmoothFade<600>>>,TrWipeSparkTip<White,300>,TrWipeIn<500>,Layers<Black,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>
 #define senateMajority InOutTr<Blast<BlastFadeout<LocalizedClash<TransitionEffect<TransitionEffect<TransitionEffect<TransitionEffect<Lockup<Mix<SwingSpeed<500>,AudioFlicker<Gradient<Gradient<BrownNoiseFlicker<Black,Orange,400>,Red>,Red,Red,Red,Red,Red,Red,Red>,Rgb<120,0,0>>,Gradient<BrownNoiseFlicker<Black,DarkOrange,400>,BrownNoiseFlicker<Black,Rgb<128,0,0>,400>,BrownNoiseFlicker<Black,Rgb<128,0,0>,400>,BrownNoiseFlicker<Black,Rgb<128,0,0>,400>>>,Pulsing<Gradient<Strobe<Black,BrownNoiseFlicker<SteelBlue,LightPink,400>,40,90>,RandomFlicker<Red,Rgb<150,20,40>>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,Red,Red>,Gradient<Strobe<Black,BrownNoiseFlicker<SteelBlue,LightPink,400>,40,90>,RandomFlicker<Red,Magenta>,Strobe<White,BrownNoiseFlicker<Red,White,100>,50,1>,RandomFlicker<Magenta,Red>,RandomFlicker<Red,Black>,RandomFlicker<Red,DarkOrange>>,3500>,RandomPerLEDFlicker<Red,White>>,White,TrWipeIn<50>,TrSmoothFade<50>,EFFECT_DRAG_END>,Gradient<Orange,Orange,White>,TrSmoothFade<50>,TrWipeIn<50>,EFFECT_DRAG_BEGIN>,Gradient<White,DeepSkyBlue,LightPink,DeepSkyBlue,White>,TrJoinR<TrWipe<200>,TrWipeIn<200>>,TrSmoothFade<300>,EFFECT_LOCKUP_END>,White,TrBoing<300,2>,TrFade<100>,EFFECT_LOCKUP_BEGIN>,BrownNoiseFlicker<White,DarkOrange,400>,100,75>,Rgb<120,0,0>>,White,200,200>,TrConcat<TrColorCycle<80,-400,-1000>,White,TrWipeIn<50>,Black,TrDelay<450>,Strobe<Black,White,45,60>,TrJoinR<TrSmoothFade<190>,TrWipe<200>>>,TrConcat<TrInstant,White,TrJoin<TrJoin<TrJoin<TrColorCycle<2050,-1500,-1>,TrBoing<2050,15>>,TrBoing<2050,18>>,TrSmoothFade<2050>>>>
 #define sithDefault InOutTr<Layers<AudioFlicker<RotateColorsX<Variation,Red>,RotateColorsX<Variation,Rgb<128,0,0>>>,TransitionEffectL<TrConcat<TrFade<200>,AlphaL<RotateColorsX<Variation,Rgb<255,150,150>>,SwingSpeed<500>>,TrDelay<30000>,AlphaL<RotateColorsX<Variation,Rgb<255,150,150>>,SwingSpeed<500>>,TrFade<800>>,EFFECT_FORCE>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Blue,300>,50,1>,TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,AlphaL<White,Bump<Int<0>,Scale<TwistAngle<>,Int<2000>,Int<10000>>>>,ResponsiveBlastFadeL<White>,ResponsiveBlastWaveL<White,300,100,300,26000,4000,EFFECT_CLASH>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<800>>,EFFECT_IGNITION>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<1000>>,EFFECT_RETRACTION>,ResponsiveStabL<OrangeRed,TrWipeIn<600>,TrWipe<600>>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,OrangeRed,Orange>,TrWipeIn<600>,TrSmoothFade<600>>>,TrWipeSparkTip<White,300>,TrWipeIn<500>,Layers<Black,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>
@@ -35,9 +31,18 @@ const unsigned int maxLedsPerStrip = 193;
 #define fulcrumDefault InOutTr<Layers<ColorChange<TrFade<1000>,AudioFlicker<Red,Rgb<128,0,0>>,AudioFlicker<Rgb<100,100,150>,Rgb<50,50,75>>>,TransitionEffectL<TrConcat<TrFade<200>,AlphaL<RotateColorsX<Variation,Rgb<255,150,150>>,SwingSpeed<500>>,TrDelay<30000>,AlphaL<RotateColorsX<Variation,Rgb<255,150,150>>,SwingSpeed<500>>,TrFade<800>>,EFFECT_FORCE>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Blue,300>,50,1>,TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,ResponsiveBlastFadeL<White>,ResponsiveBlastWaveL<White,300,100,300,26000,4000,EFFECT_CLASH>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<1000>>,EFFECT_RETRACTION>,ResponsiveStabL<OrangeRed,TrWipeIn<600>,TrWipe<600>>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,OrangeRed,Orange>,TrWipeIn<600>,TrSmoothFade<600>>>,TrWipe<300>,TrWipeIn<500>,Layers<Black,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>
 #define powerSurge InOutTr<Layers<AudioFlicker<RotateColorsX<Variation,Rgb<0,128,0>>,RotateColorsX<Variation,Rgb<0,200,0>>>,TransitionLoopL<TrWaveX<HumpFlickerL<RotateColorsX<Variation,Rgb<50,255,50>>,40>,Int<250>,Int<100>,Int<200>,Int<0>>>,TransitionLoopL<TrWaveX<HumpFlickerL<RotateColorsX<Variation,Green>,40>,Int<350>,Int<100>,Int<300>,Int<0>>>,TransitionEffectL<TrConcat<TrFade<400>,Mix<SwingSpeed<600>,AudioFlickerL<Rgb<150,0,0>>,Red>,TrDelay<30000>,Mix<SwingSpeed<600>,AudioFlickerL<Rgb<150,0,0>>,Red>,TrFade<800>>,EFFECT_FORCE>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Red,300>,50,1>,TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,AlphaL<White,Bump<Int<0>,Scale<TwistAngle<>,Int<2000>,Int<10000>>>>,ResponsiveBlastFadeL<White>,ResponsiveBlastWaveL<White,300,100,300,26000,4000,EFFECT_CLASH>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<800>>,EFFECT_IGNITION>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<1000>>,EFFECT_RETRACTION>,ResponsiveStabL<Red,TrWipeIn<600>,TrWipe<600>>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,Red,Orange>,TrWipeIn<600>,TrSmoothFade<600>>>,TrWipeSparkTip<White,300>,TrWipeIn<500>,Layers<Black,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>
 
+#define mainColors ColorChange<TrWipe<500>,Rgb<100,100,150>,GreenYellow,Chartreuse,Green,SpringGreen,Cyan,DeepSkyBlue,DodgerBlue,Blue,Violet,Indigo,Magenta,HotPink,Red,OrangeRed,DarkOrange,Orange,Rgb<255,160,0>,Yellow>
+#define flickerColors ColorChange<TrWipe<500>,Rgb<50,50,75>,Rgb<54,128,3>,Rgb<28,128,0>,Rgb<0,128,0>,SpringGreenDark,DeepSkyBlue,DeepSkyBlueDark,DodgerBlueDark,Rgb<0,0,128>,VioletDark,IndigoDark,MagentaDark,Pink,Rgb<128,0,0>,Rgb<128,14,0>,Rgb<128,34,0>,Rgb<128,48,0>,Rgb<84,60,0>,Rgb<128,128,0>>
+#define colorsBright ColorChange<TrWipe<500>,Rgb<170,170,220>,Rgb<200,255,150>,Rgb<155,255,100>,Rgb<150,255,150>,Rgb<100,255,155>,LightCyan,Rgb<100,235,255>,Rgb<102,200,255>,Rgb<100,100,255>,Rgb<203,90,255>,Rgb<200,100,255>,Rgb<255,100,255>,LightPink,Rgb<255,150,150>,Rgb<255,168,100>,Rgb<255,114,50>,Rgb<255,197,100>,Rgb<255,255,150>,Rgb<255,255,150>>
+#define clashAccent ColorChange<TrWipe<500>,Red,Red,Red,Red,Red,Red,Red,Red,Red,Red,Red,Red,Magenta,Orange,Yellow,Yellow,Red,Red,Red>
+#define meltAccent ColorChange<TrWipe<500>,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Orange,Yellow,Yellow,Yellow,Yellow,Orange,Orange>
+#define colorsBright ColorChange<TrWipe<500>,Rgb<170,170,220>,Rgb<200,255,150>,Rgb<155,255,100>,Rgb<150,255,150>,Rgb<100,255,155>,LightCyan,Rgb<100,235,255>,Rgb<102,200,255>,Rgb<100,100,255>,Rgb<203,90,255>,Rgb<200,100,255>,Rgb<255,100,255>,LightPink,Rgb<255,150,150>,Rgb<255,168,100>,Rgb<255,114,50>,Rgb<255,197,100>,Rgb<255,255,150>,Rgb<255,255,150>>
+#define flareColors ColorChange<TrWipe<500>,White,Yellow,Yellow,Green,SpringGreen,White,White,Cyan,Cyan,White,White,Indigo,Magenta,Orange,Yellow,Rgb<255,160,0>,Yellow,YellowGreen,YellowGreen>
+#define mainBladeStyle InOutTr<Layers<AudioFlicker<mainColors,flickerColors>,TransitionEffectL<TrConcat<TrFade<200>,AlphaL<colorsBright,SwingSpeed<500>>,TrDelay<30000>,AlphaL<colorsBright,SwingSpeed<500>>,TrFade<800>>,EFFECT_FORCE>,TransitionEffectL<TrConcat<TrInstant,BrownNoiseFlickerL<AlphaL<White,Int<16000>>,Int<50>>,TrSmoothFade<600>>,EFFECT_LOCKUP_END>,ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,clashAccent,300>,50,1>,TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>>,ResponsiveLightningBlockL<Strobe<White,AudioFlicker<White,Blue>,50,1>,TrConcat<TrInstant,AlphaL<White,Bump<Int<12000>,Int<18000>>>,TrFade<200>>,TrConcat<TrInstant,HumpFlickerL<AlphaL<White,Int<16000>>,30>,TrSmoothFade<600>>>,AlphaL<flareColors,Bump<Int<0>,Scale<TwistAngle<>,Int<2000>,Int<10000>>>>,ResponsiveBlastFadeL<White>,ResponsiveBlastWaveL<White,300,100,300,26000,4000,EFFECT_CLASH>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<800>>,EFFECT_IGNITION>,TransitionEffectL<TrConcat<TrInstant,AudioFlickerL<White>,TrFade<1000>>,EFFECT_RETRACTION>,ResponsiveStabL<clashAccent,TrWipeIn<600>,TrWipe<600>>,ResponsiveDragL<BrownNoiseFlickerL<White,Int<300>>,TrWipeIn<400>,TrFade<400>>,ResponsiveMeltL<Mix<TwistAngle<>,clashAccent,meltAccent>,TrWipeIn<600>,TrSmoothFade<600>>>,TrWipeSparkTip<White,300>,TrWipeIn<500>,Layers<Black,TransitionEffectL<TrConcat<TrDelay<1500>,Black,TrFade<1000>,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_BOOT>,TransitionEffectL<TrConcat<TrInstant,AlphaL<Mix<BatteryLevel,Red,Green>,Bump<Int<0>,Int<6000>>>,TrFade<3000>>,EFFECT_NEWFONT>>>
+
 Preset blade[] = {
   { "Luke_ESB", "Luke_ESB/tracks/LvsV.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "Luke_ESB"
   },
   { "Unstable", "tracks/KyloTheme.wav",
@@ -53,11 +58,11 @@ Preset blade[] = {
     StylePtr<chassisOff>(), "Fulcrum"
   },
   { "Hero_Obi", "Hero_Obi/tracks/BattleOfHeros.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "Hero_Obi"
   },
   { "Luke_RoTJ", "Luke_RoTJ/tracks/Final_Duel.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "Luke_RoTJ"
   },
   { "ClassicVillian", "Luke_ESB/tracks/LvsV.wav",
@@ -73,31 +78,31 @@ Preset blade[] = {
     StylePtr<chassisOff>(), "Senate Majority"
   },
   { "FOBlue", "tracks/JFO1.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOBlue"
   },
   { "FOCyan", "tracks/JFO2.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOCyan"
   },
   { "FOGreen", "tracks/JFO6.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOGreen"
   },
   { "FOIndigo", "tracks/JFO8.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOIndigo"
   },
   { "FOMagenta", "tracks/JFO4.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOMagenta"
   },
   { "FOOrange", "tracks/JFO3.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOOrange"
   },
   { "FOPurple", "tracks/JFO14.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOPurple"
   },
   { "FORed", "tracks/JFO19.wav",
@@ -105,7 +110,7 @@ Preset blade[] = {
     StylePtr<chassisOff>(), "FORed"
   },
   { "FOYellow", "tracks/JFO7.wav",
-    StylePtr<jediDefault>(),
+    StylePtr<mainBladeStyle>(),
     StylePtr<chassisOff>(), "FOYellow"
   },
   { "EtaanCrystal", "tracks/JF011.wav",
@@ -116,8 +121,8 @@ Preset blade[] = {
 
 Preset chassis[] = {
   { "Luke_ESB", "Luke_ESB/tracks/LvsV.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "Luke_ESB"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "Luke_ESB"
   },
   { "Unstable", "tracks/KyloTheme.wav",
     StylePtr<unstableStyle>(),
@@ -132,12 +137,12 @@ Preset chassis[] = {
     StylePtr<fulcrumDefault>(), "Fulcrum"
   },
   { "Hero_Obi", "Hero_Obi/tracks/BattleOfHeros.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "Hero_Obi"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "Hero_Obi"
   },
   { "Luke_RoTJ", "Luke_RoTJ/tracks/Final_Duel.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "Luke_RoTJ"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "Luke_RoTJ"
   },
   { "ClassicVillian", "Luke_ESB/tracks/LvsV.wav",
     StylePtr<sithDefault>(),
@@ -152,40 +157,40 @@ Preset chassis[] = {
     StylePtr<senateMajority>(), "Senate Majority"
   },
   { "FOBlue", "tracks/JFO1.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOBlue"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOBlue"
   },
   { "FOCyan", "tracks/JFO2.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOCyan"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOCyan"
   },
   { "FOGreen", "tracks/JFO6.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOGreen"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOGreen"
   },
   { "FOIndigo", "tracks/JFO8.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOIndigo"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOIndigo"
   },
   { "FOMagenta", "tracks/JFO4.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOMagenta"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOMagenta"
   },
   { "FOOrange", "tracks/JFO3.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOOrange"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOOrange"
   },
   { "FOPurple", "tracks/JFO14.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOPurple"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOPurple"
   },
   { "FORed", "tracks/JFO19.wav",
     StylePtr<sithDefault>(),
     StylePtr<sithDefault>(), "FORed"
   },
   { "FOYellow", "tracks/JFO7.wav",
-    StylePtr<jediDefault>(),
-    StylePtr<jediDefault>(), "FOYellow"
+    StylePtr<mainBladeStyle>(),
+    StylePtr<mainBladeStyle>(), "FOYellow"
   },
   { "EtaanCrystal", "tracks/JF011.wav",
     StylePtr<powerSurge>(),
