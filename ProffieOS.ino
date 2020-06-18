a9b4dfc2dde416e1b1f995540605a70a2f497d20
Nick Daigle <nickdaigle66@gmail.com>
2020-06-18 13:12:57 -0500

    Add Quigon saber config

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -22,8 +22,9 @@
 // to use here.
 
 //#define CONFIG_FILE "config/graflex_neo_config.h"
-// #define CONFIG_FILE "config/disciple_neo_config.h"
-#define CONFIG_FILE "config/fallen_neo_fo_config.h"
+//#define CONFIG_FILE "config/disciple_neo_config.h"
+//#define CONFIG_FILE "config/fallen_neo_fo_config.h"
+#define CONFIG_FILE "config/quigon_neo_config.h"
 // #define CONFIG_FILE "config/fallen_tri_config.h"
 // #define CONFIG_FILE "config/fallen_tri_fo_config.h"
 // #define CONFIG_FILE "config/default_proffieboard_config.h"
